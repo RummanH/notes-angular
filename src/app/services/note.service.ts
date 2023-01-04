61ff22ecc46b13967539210a8ab5d9eea89f19ac
rummanH <jah.hasant@gmail.com>
2023-01-04 20:39:05 +0600

    final

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -11,21 +11,6 @@ export class NoteService {
   constructor(private http: HttpClient, private router: Router) {}
   private apiURL = 'http://localhost:8000/notes';
 
-  signup(user: User): any {
-    let exist = false;
-    this.http.get<User>(this.apiURL).subscribe((users) => {
-      //@ts-ignore
-      const currentUser = users.find((u) => u.email === user.email);
-      if (currentUser) {
-        alert('A user with this email address already exist!');
-        return;
-      } else {
-        this.http.post<User>(this.apiURL, user).subscribe();
-        this.router.navigate(['login']);
-      }
-    });
-  }
-
   deleteNote(note: any) {
     return this.http.delete<any>(`${this.apiURL}/${note.id}`);
   }
